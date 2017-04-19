9c53364e3af46e5e89fc5bd1af400b15f716a494
jessHwood <jhorntvedt@gmail.com>
2017-04-19 12:23:08 -0600

    add login and signup functionality

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+module.exports = function(passport) {
+
+	passport.serializeUser(function(user, callback){
+		callback(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, callback){
+		User.findById(id, function(err, user){
+			callback(err, user);
+		});
+	});
+
+	passport.use('local-signup', new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback : true
+	}, function(req, email, password, callback) {
+		//find a user with this e-mail
+	User.findOne({'local.email': email}, function(err, user) {
+		if(err) return callback(err);
+
+		//if there is already a user with this email
+		if(user) {
+			return callback(null, false, req.flash('signupMessage', 'This email has already been used.'));
+		} else {
+			//there is no user registered with this email
+			//create a new user
+			var newUser = new User();
+			newUser.local.email = email;
+			newUser.local.password = newUser.encrypt(password);
+
+			newUser.save(function(err){
+				if(err) throw err;
+				return callback(null, newUser);
+			});
+		}
+	});
+	}));
+};
---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+var passport = require("passport")
+
+// GET /signup
+function getSignup(request, response, next) {
+	response.render('signup.ejs', {message:request.flash('signupMessage')});
+}
+
+//POST /signup
+function postSignup(request, response, next) {
+	var signupStrategy = passport.authenticate('local-signup', {
+		successRedirect: '/',
+		failureRedirect: '/signup', 
+		failureFlash: true
+	});
+
+	return signupStrategy(request, response, next);
+}
+
+// GET /login
+function getLogin(request, response) { 
+	response.render('login.ejs', {message:request.flash('loginMessage')});
+}
+
+// POST /login 
+function postLogin(request, response, next) {
+	var loginStrategy = passport.authenticate('local-logins', {
+		successRedirect: '/', 
+		failureRedirect: '/login', 
+		failureFlash: true
+	});
+
+	return loginStrategy(request, response, next);
+}
+
+// GET /logout
+function getLogout(request, response, next) { 
+	request.logout();
+	response.redirect('/');
+}
+
+// Restricted page
+function secret(request, response, next){
+	response.json("message: super secret ninja");
+}
+
+module.exports = {
+  getLogin: getLogin,
+  postLogin: postLogin ,
+  getSignup: getSignup,
+  postSignup: postSignup,
+  getLogout: getLogout,
+  secret: secret
+};

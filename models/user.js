fbb63ad5492c239066cf6a24a2f6b83aa2683038
jessHwood <jhorntvedt@gmail.com>
2017-04-19 16:43:11 -0600

    add bootstrap styles and photos

--- models/user.js
+++ models/user.js
@@ -12,5 +12,9 @@ User.methods.encrypt = function(password) {
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 };
 
+User.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
+
 
 module.exports = mongoose.model('User', User);

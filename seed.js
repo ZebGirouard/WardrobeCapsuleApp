d8bfb484f385899de6316785d2282bd0c4fa9dc4
jessHwood <jhorntvedt@gmail.com>
2017-04-24 10:06:41 -0600

    adding heroku seed

--- seed.js
+++ seed.js
@@ -22,12 +22,48 @@ var db = require('./models');
 
 //get route for ShopStyle api dresses - returns 50 dresses(max per api) and makes sure price is less than $200
 
-request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=dress&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
-	var dressList = [];
+// request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=dress&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
+// 	var dressList = [];
+// 	body = JSON.parse(body);
+// 	// console.log(body);
+// 	body.products.forEach(function(product){
+// 		dressObj = {
+// 		name: product.brandedName,
+// 		price: product.priceLabel,
+// 		retailer: product.retailer.name,
+// 		description: product.description, 
+// 		url: product.clickUrl, 
+// 		image: product.image.sizes.IPhone.url
+// 		};
+// 		dressList.push(dressObj);
+// 		// console.log(dressList);
+// 	});
+
+// 	db.Dress.remove({}, function(err, dresses){
+// 		// console.log(dressList);
+// 		if (err) {
+// 			console.log('error occurred in remove', err);
+// 		} else {
+// 			console.log('removed all dresses');
+// 		}
+// 	});
+// 		//create new records based on dress array
+// 		db.Dress.create(dressList, function(err, dresses){
+// 			if (err) { return console.log('err', err); }
+// 			console.log("created", dressList.length, "dresses");
+// 			process.exit();
+// 		});
+
+// });
+
+// get route for ShopStyle api tops - returns 50 tops(max per api) and makes sure price is less than $200
+
+request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=top&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
+	var topList = [];
 	body = JSON.parse(body);
 	// console.log(body);
 	body.products.forEach(function(product){
-		dressObj = {
+		topObj = {
 		name: product.brandedName,
 		price: product.priceLabel,
 		retailer: product.retailer.name,
@@ -35,134 +71,98 @@ request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=dress&o
 		url: product.clickUrl, 
 		image: product.image.sizes.IPhone.url
 		};
-		dressList.push(dressObj);
-		// console.log(dressList);
+		topList.push(topObj);
+	
 	});
 
-	db.Dress.remove({}, function(err, dresses){
+	db.Top.remove({}, function(err, tops){
 		// console.log(dressList);
 		if (err) {
 			console.log('error occurred in remove', err);
 		} else {
-			console.log('removed all dresses');
+			console.log('removed all tops');
 		}
 	});
 		//create new records based on dress array
-		db.Dress.create(dressList, function(err, dresses){
+		db.Top.create(topList, function(err, tops){
 			if (err) { return console.log('err', err); }
-			console.log("created", dressList.length, "dresses");
+			console.log("created", topList.length, "tops");
 			process.exit();
 		});
 
 });
 
-//get route for ShopStyle api tops - returns 50 tops(max per api) and makes sure price is less than $200
-
-// request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=top&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
-// 	var topList = [];
-// 	body = JSON.parse(body);
-// 	// console.log(body);
-// 	body.products.forEach(function(product){
-// 		topObj = {
-// 		name: product.brandedName,
-// 		price: product.priceLabel,
-// 		retailer: product.retailer.name,
-// 		description: product.description, 
-// 		url: product.clickUrl, 
-// 		image: product.image.sizes.IPhone.url
-// 		};
-// 		topList.push(topObj);
-	
-// 	});
-
-// 	db.Top.remove({}, function(err, tops){
-// 		// console.log(dressList);
-// 		if (err) {
-// 			console.log('error occurred in remove', err);
-// 		} else {
-// 			console.log('removed all tops');
-// 		}
-// 	});
-// 		//create new records based on dress array
-// 		db.Top.create(topList, function(err, tops){
-// 			if (err) { return console.log('err', err); }
-// 			console.log("created", topList.length, "tops");
-// 			process.exit();
-// 		});
-
-// });
-
 // get route for ShopStyle api pants - returns 50 pants(max per api) and makes sure price is less than $150
 
-// request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=womens-pants&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
-// 	var pantList = [];
-// 	body = JSON.parse(body);
-// 	// console.log(body);
-// 	body.products.forEach(function(product){
-// 		pantObj = {
-// 		name: product.brandedName,
-// 		price: product.priceLabel,
-// 		retailer: product.retailer.name,
-// 		description: product.description, 
-// 		url: product.clickUrl, 
-// 		image: product.image.sizes.IPhone.url
-// 		};
-// 		pantList.push(pantObj);
+request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=womens-pants&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
+	var pantList = [];
+	body = JSON.parse(body);
+	// console.log(body);
+	body.products.forEach(function(product){
+		pantObj = {
+		name: product.brandedName,
+		price: product.priceLabel,
+		retailer: product.retailer.name,
+		description: product.description, 
+		url: product.clickUrl, 
+		image: product.image.sizes.IPhone.url
+		};
+		pantList.push(pantObj);
 	
-// 	});
+	});
 
-// 	db.Pant.remove({}, function(err, pants){
-// 		// console.log(dressList);
-// 		if (err) {
-// 			console.log('error occurred in remove', err);
-// 		} else {
-// 			console.log('removed all pants');
-// 		}
-// 	});
-// 		//create new records based on dress array
-// 		db.Pant.create(pantList, function(err, pants){
-// 			if (err) { return console.log('err', err); }
-// 			console.log("created", pantList.length, "pants");
-// 			process.exit();
-// 		});
+	db.Pant.remove({}, function(err, pants){
+		// console.log(dressList);
+		if (err) {
+			console.log('error occurred in remove', err);
+		} else {
+			console.log('removed all pants');
+		}
+	});
+		//create new records based on dress array
+		db.Pant.create(pantList, function(err, pants){
+			if (err) { return console.log('err', err); }
+			console.log("created", pantList.length, "pants");
+			process.exit();
+		});
 
-// });
+});
 
 // get route for ShopStyle api shoes - returns 50 shoes(max per api) and makes sure price is less than $150(supposed to do this - its not)
 
-// request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=womens-shoes&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
-// 	var shoeList = [];
-// 	body = JSON.parse(body);
-// 	// console.log(body);
-// 	body.products.forEach(function(product){
-// 		shoeObj = {
-// 		name: product.brandedName,
-// 		price: product.priceLabel,
-// 		retailer: product.retailer.name,
-// 		description: product.description, 
-// 		url: product.clickUrl, 
-// 		image: product.image.sizes.IPhone.url
-// 		};
-// 		shoeList.push(shoeObj);
+request('http://api.shopstyle.com/api/v2/products?pid=' + apiKey + '&fts=womens-shoes&offset=0&limit=50&fl=p7&fl=p8&fl=p9&fl=p10', function(error, response, body){
+	var shoeList = [];
+	body = JSON.parse(body);
+	// console.log(body);
+	body.products.forEach(function(product){
+		shoeObj = {
+		name: product.brandedName,
+		price: product.priceLabel,
+		retailer: product.retailer.name,
+		description: product.description, 
+		url: product.clickUrl, 
+		image: product.image.sizes.IPhone.url
+		};
+		shoeList.push(shoeObj);
 	
-// 	});
+	});
 
-// 	db.Shoe.remove({}, function(err, shoes){
+	db.Shoe.remove({}, function(err, shoes){
 		
-// 		if (err) {
-// 			console.log('error occurred in remove', err);
-// 		} else {
-// 			console.log('removed all shoes');
-// 		}
-// 	});
-// 		//create new records based on dress array
-// 		db.Shoe.create(shoeList, function(err, shoes){
-// 			if (err) { return console.log('err', err); }
-// 			console.log("created", shoeList.length, "shoes");
-// 			process.exit();
-// 		});
+		if (err) {
+			console.log('error occurred in remove', err);
+		} else {
+			console.log('removed all shoes');
+		}
+	});
+		//create new records based on dress array
+		db.Shoe.create(shoeList, function(err, shoes){
+			if (err) { return console.log('err', err); }
+			console.log("created", shoeList.length, "shoes");
+			process.exit();
+		});
 
-// });
+});
 
 
 
